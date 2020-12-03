6960fa45e71afcfba7fd94c0dfa7579b69d31667
mpurdy1 <mpurdy1@tennessee.edu>
2020-12-03 10:27:59 -0500

    Fixing some color issues.

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'UTKDS_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'UTKDS_VERSION', '0.0.10' );
+	define( 'UTKDS_VERSION', '0.0.1012' );
 }
 
 if ( ! function_exists( 'ut_ds_setup' ) ) :
@@ -215,9 +215,9 @@ function ut_designsystem_scripts() {
 	    wp_enqueue_script('jquery');
     }
 
-//    wp_enqueue_style( 'utk-bootstrap-designsytemstyles',    'http://images.utk.edu/designsystem/2020/assets/css/style.css', array(), UTKDS_VERSION );
+//    wp_enqueue_style( 'utk-bootstrap-designsytemstyles',    'https://images.utk.edu/designsystem/2020/assets/css/style.css', array(), UTKDS_VERSION );
 
-    wp_enqueue_style( 'utk-bootstrap-designsytemstyles',    'https://images.utk.edu/designsystem/2020/assets/css/style.css', array(), UTKDS_VERSION );
+    wp_enqueue_style( 'utk-bootstrap-designsytemstyles',    'http://localhost/design-system-build-files/build/assets/css/style.css', array(), UTKDS_VERSION );
    	wp_enqueue_script( 'utk-bootstrap-designsytemscripts',  get_template_directory_uri() . '/js/utk.js', array( 'jquery' ), UTKDS_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'ut_designsystem_scripts' );
--- src/template-parts/region-extendedfooter.php
+++ src/template-parts/region-extendedfooter.php
@@ -1,39 +1,41 @@
-<?php if ( is_active_sidebar( 'sidebar-2' ) || is_active_sidebar( 'sidebar-3' ) ||	is_active_sidebar( 'sidebar-4' ) ||	is_active_sidebar( 'sidebar-5' )	) : ?>
+<?php
+  $site_width = get_theme_mod( 'site_width' );
+  if ( is_active_sidebar( 'sidebar-2' ) || is_active_sidebar( 'sidebar-3' ) ||	is_active_sidebar( 'sidebar-4' ) ||	is_active_sidebar( 'sidebar-5' )	) : ?>
 
-  <div class="container-fluid"> 
+<div class="container<?php if ($site_width == "full-width") { ?>-fluid<?php }; ?>"> 
 <div id="extended-footer" class="row">
 
-<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-  <div id="second" class="col-lg" role="complementary">
-    <?php dynamic_sidebar( 'sidebar-2' ); ?>
-  </div>
-  <!-- #second .widget-area -->
-<?php endif; ?>
+  <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+    <div id="second" class="col-lg" role="complementary">
+      <?php dynamic_sidebar( 'sidebar-2' ); ?>
+    </div>
+    <!-- #second .widget-area -->
+  <?php endif; ?>
+  
+  
+  <?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
+    <div id="third" class="col-lg" role="complementary">
+       <?php dynamic_sidebar( 'sidebar-3' ); ?>
+    </div>
+    <!-- #third .widget-area -->
+  <?php endif; ?>
+  
+  
+  <?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+    <div id="third" class="col-lg" role="complementary">
+       <?php dynamic_sidebar( 'sidebar-4' ); ?>
+    </div>
+    <!-- #third .widget-area --> 
+  <?php endif; ?>
+  
+  <?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
+    <div id="third" class="col-lg" role="complementary">
+       <?php dynamic_sidebar( 'sidebar-5' ); ?>
+    </div>
+    <!-- #third .widget-area -->
+  <?php endif; ?>
 
-
-<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
-  <div id="third" class="col-lg" role="complementary">
-     <?php dynamic_sidebar( 'sidebar-3' ); ?>
-  </div>
-  <!-- #third .widget-area -->
-<?php endif; ?>
-
-
-<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
-  <div id="third" class="col-lg" role="complementary">
-     <?php dynamic_sidebar( 'sidebar-4' ); ?>
-  </div>
-  <!-- #third .widget-area --> 
-<?php endif; ?>
-
-<?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
-  <div id="third" class="col-lg" role="complementary">
-     <?php dynamic_sidebar( 'sidebar-5' ); ?>
-  </div>
-  <!-- #third .widget-area -->
-<?php endif; ?>
-  </div>
-
+</div>
 </div>
 <!-- #linkdrawer -->
 <?php endif; ?>
