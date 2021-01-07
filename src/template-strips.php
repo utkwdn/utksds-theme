7cc659f39555e6f13f28968a26d78d955d925215
mpurdy1 <mpurdy1@tennessee.edu>
2021-01-07 16:47:41 -0500

    New template for displaying strips from edge to edge.

---
+++ src/template-strips.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Strips
+ * Description: Full-width, breaks out of the grid (no max-width) for using with the Strips block.
+ *
+ * @package WordPress
+ * @subpackage utresponsive
+ */
+
+
+get_header();
+$site_width = get_theme_mod( 'site_width' );
+$menu_type = get_theme_mod( 'menu_type' );
+$site_homeheadline = get_theme_mod( 'site_homeheadline' );
+
+get_template_part( 'template-parts/part-startheader' );
+ut_ds_post_thumbnail();
+get_template_part( 'template-parts/inc-breadcrumb' ); 
+
+// Is this the front page? And is this the switch to show the home page headline flipped?
+if (is_front_page() && $site_homeheadline=="hide"){
+
+} else {
+  the_title( '<header class="entry-header col-12"><h1 class="entry-title">', '</h1></header>' );
+}
+?>
+<!-- .entry-header -->
+<?php
+  get_template_part( 'template-parts/part-endheader' );
+ // get_template_part( 'template-parts/nav-left-rail' );
+
+while ( have_posts() ) :
+  the_post();
+  get_template_part( 'template-parts/content', 'pagenoside' );
+endwhile; // End of the loop.
+?>
+
+<?php
+get_footer();
