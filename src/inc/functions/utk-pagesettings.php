7578f5f399d6151904a391a9dfa7a57295e6e042
mpurdy1 <mpurdy1@tennessee.edu>
2021-05-11 16:55:44 -0400

    The headline area can now be hidden per page.

---
+++ src/inc/functions/utk-pagesettings.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/*********************************************************************/
+/* Add featured post checkbox
+/********************************************************************/
+add_action( 'add_meta_boxes', 'add_featured_checkbox_function' );
+function add_featured_checkbox_function() {
+   add_meta_box('featured_checkbox_id','Page layout', 'featured_checkbox_callback_function', 'page', 'side',  'high');
+}
+function featured_checkbox_callback_function( $post ) {
+   global $post;
+   $showsHeadline=get_post_meta( $post->ID, 'shows_headline', true );
+?>
+   
+   <input type="checkbox" id="shows_headline" name="shows_headline" value="yes" <?php echo (($showsHeadline=='yes') ? 'checked="checked"': '');?>/> <label for="shows_headline"> Hide the title area.</label>
+<?php
+}
+
+add_action('save_post', 'save_featured_post'); 
+function save_featured_post($post_id){ 
+   update_post_meta( $post_id, 'shows_headline', $_POST['shows_headline']);
+}
