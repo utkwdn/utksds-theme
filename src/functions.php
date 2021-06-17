6757624d42a515f4cb4cc2ef658acc943b7cf134
mpurdy1 <mpurdy1@tennessee.edu>
2021-06-17 13:17:41 -0400

    Page hidden switched to visually-hidden

--- src/functions.php
+++ src/functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'UTKDS_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'UTKDS_VERSION', '0.0.16' );
+	define( 'UTKDS_VERSION', '0.0.17' );
 }
 
 if ( ! function_exists( 'ut_ds_setup' ) ) :
--- src/template-parts/region-headsearch.php
+++ src/template-parts/region-headsearch.php
@@ -20,11 +20,11 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
+<div class="modal fade mt-5" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-fullscreen-sm-down modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="searchModalLabel">Modal title</h5>
+
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
