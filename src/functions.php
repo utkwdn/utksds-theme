84921c8ec1205919d1bb18d15bca8c6d1d26ecc1
Charlie Cantrell <ccantr19@utk.edu>
2021-12-15 13:21:49 -0500

    Tweaked the autoupdate implementation to make it more reliable.

--- src/functions.php
+++ src/functions.php
@@ -15,7 +15,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('main');
+//$myUpdateChecker->setBranch('main');
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
 
 if ( ! defined( 'UTKDS_VERSION' ) ) {
